bfe25a3a82a122b267245e2341faf54beed023e0
Daniel Gomez <dgomez.informatica@gmail.com>
2023-04-06 15:21:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Numero Par e Impar
+
+Ejercicio practico para saber si un numero es par o impar
+
+> Nota! Este proyecto se realizo con visual studio 2022, compatible con visual estudo 2019
+
+## License
+
+**Free Software | MIT**
---
+++ aiep_numero_par_impar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aiep_numero_par_impar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("\nPres and key continue...");
+            Console.ReadKey();
+        }
+    }
+}
