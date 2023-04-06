90a7bea49c990d058fa164702fa8cd788d3cbad3
Daniel Gomez <dgomez.informatica@gmail.com>
2023-04-06 16:04:25 -0400

    tarea completa, add metodo comprobador de pares

--- aiep_numero_par_impar/Program.cs
+++ aiep_numero_par_impar/Program.cs
@@ -6,10 +6,37 @@ namespace aiep_numero_par_impar
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ingrese el número por favor");
+            int numeroUsuario = int.Parse(Console.ReadLine());
 
+            if (comprobadorDePares(numeroUsuario))
+            {
+                Console.WriteLine("Es Par");
+            } 
+            else
+            {
+                Console.WriteLine("No es Impar");
+            }
 
             Console.WriteLine("\nPres and key continue...");
             Console.ReadKey();
+        }
+
+
+        static bool comprobadorDePares(int numero)
+        {
+            bool esPar;
+
+            if ((numero % 2) == 0)
+            {
+                esPar = true;
+            }
+            else
+            {
+                esPar = false;
+            }
+
+            return esPar;
         }
     }
 }
